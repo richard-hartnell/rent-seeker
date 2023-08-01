2333999be77582e171b6006d8d84c86e3bd3eb2c
unknown <richard@richardhartnell.com>
2023-07-31 18:59:10 -0700

    Bugfix telling posts from comments

--- reply_post.py
+++ reply_post.py
@@ -7,15 +7,17 @@ import os
 #inits
 commentIterator = 0
 searchTerm = "meatbag"
-subredditOfInterest = 'pythonforengineers'
+subs = ['pythonforengineers']
+comments_found = []
 
 # the reply
-reply_text = "Hi! I'm clown_b0t, part of the circus. I roam Reddit trying to clear up three important things about this too-frequent comparison:\n \n" \
-             "1. Clowns are very diligent and work exceedingly hard at refining their art.\n \n"\
+reply_text = "Hi! Circus performer here. This account roams Reddit trying to clear up three important things about this too-frequent comparison:\n \n" \
+             "1. Clowns are very diligent and work very hard at refining their art.\n \n"\
              "2. Clowns are generally very kind and well-intentioned people.\n \n" \
              "3. Clowns are only *pretending* they are completely stupid.\n \n" \
-             "I'm a bot! If anything's wrong, just reply to this comment." \
-             "[Dario Fo](https://en.wikipedia.org/wiki/Dario_Fo), " + "the only clown to win a Nobel Prize in Literature." + "https://www.youtube.com/watch?v=TqKfwC70YZI"
+             "^Your comment was searched for automatically, but this post was made manually." \
+             "^For a clownish rabbit hole, please enjoy an antifascist play by [Dario Fo](https://en.wikipedia.org/wiki/Dario_Fo), " + "the only clown to win a Nobel Prize in Literature." + "https://www.youtube.com/watch?v=TqKfwC70YZI"
+reply_text_2 = "Test comment."
 
 # Create the Reddit instance
 reddit = praw.Reddit('bot1')
@@ -24,41 +26,36 @@ reddit = praw.Reddit('bot1')
 # with open("creds.txt", "r") as f:
 #     reddit.login(f[0], f[1])
 
-# Have we run this code before? If not, create an empty list
-if not os.path.isfile("posts_replied_to.txt"):
-    posts_replied_to = []
+# if not os.path.isfile("comments_found.txt"):
+#     comments_found = []
 
-# If we have run the code before, load the list of posts we have replied to
-else:
-    # Read the file into a list and remove any empty values
-    with open("posts_replied_to.txt", "r") as f:
-        posts_replied_to = f.read()
-        posts_replied_to = posts_replied_to.split("\n")
-        posts_replied_to = list(filter(None, posts_replied_to))
+with open("comments_found.txt", "r") as f:
+    comments_found = f.read()
+    comments_found = comments_found.split("\n")
+    comments_found = list(filter(None, comments_found))
 
 # let's pull comments from some subs!
-subreddit = reddit.subreddit(subredditOfInterest)
-for submission in subreddit.hot(limit=10):
-    commentIterator = 0
-    print("Scanning post in /r/" + subreddit.display_name + ": " + submission.title)
-    submission.comments.replace_more(limit=4)
-    for comment in submission.comments.list():
-        if commentIterator <= 12 and (str(searchTerm) in str(comment.body.lower())):
-            commentIterator += 1
-            print("Comment " + str(commentIterator) + ": " + comment.body.lower())
-            thisComment = reddit.comment(comment.id)
-            thisComment.reply(body="It's still working!")
-            print("I think it worked.")
-        else:
-            pass
+for sub in subs:
+    subreddit = reddit.subreddit(sub)
+    for submission in subreddit.hot(limit=10):
+        commentIterator = 0
+        print("\n Scanning post in /r/" + subreddit.display_name + ": " + submission.title)
+        submission.comments.replace_more(limit=4)
+        for comment in submission.comments.list():
+            if commentIterator <= 12 and (str(searchTerm) in str(comment.body.lower())):
+                commentIterator += 1
+                print("Comment " + str(commentIterator) + ": " + comment.body.lower())
+                thisComment = reddit.comment(comment.id)
+                if comment.id not in comments_found:
+                    # thisComment.reply(reply_text_2)
+                    # print("Test submission")
+                    print("Comment id:" + comment.id)
+                    comments_found.append(comment.id)
+                # thisComment.reply(body="It's still working!") // here's the reaction
+            else:
+                pass
 
-#     if submission.id not in posts_replied_to:
-#             # submission.reply(reply_text)
-#             print("Found: ", submission.title)
-#             # Store the current id into our list
-#             posts_replied_to.append(submission.id)
-
-# Write our updated list back to the file
-with open("posts_replied_to.txt", "w") as f:
-    for post_id in posts_replied_to:
-        f.write(post_id + "\n")
+    # Write our updated list back to the file
+    with open("comments_found.txt", "w") as f:
+        for comment_id in comments_found:
+            f.write(comment_id + "\n")
