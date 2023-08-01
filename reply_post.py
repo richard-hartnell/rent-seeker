8d1de420953f5ad45ea86964da09983dcd659a37
richard-hartnell <richard@richardhartnell.com>
2023-08-01 12:00:30 -0700

    Now takes user input to leave comment

--- reply_post.py
+++ reply_post.py
@@ -15,19 +15,13 @@ reply_text = "Hi! Circus performer here. This account roams Reddit trying to cle
              "1. Clowns are very diligent and work very hard at refining their art.\n \n"\
              "2. Clowns are generally very kind and well-intentioned people.\n \n" \
              "3. Clowns are only *pretending* they are completely stupid.\n \n" \
-             "^Your comment was searched for automatically, but this post was made manually." \
-             "^For a clownish rabbit hole, please enjoy an antifascist play by [Dario Fo](https://en.wikipedia.org/wiki/Dario_Fo), " + "the only clown to win a Nobel Prize in Literature." + "https://www.youtube.com/watch?v=TqKfwC70YZI"
-reply_text_2 = "Test comment."
+             "-- \n \n" \
+             "^This account is only kind of a bot. Your comment was searched for automatically, but this post was made manually." \
+             "^For a clownish rabbit hole, please enjoy this play written by [Dario Fo](https://en.wikipedia.org/wiki/Dario_Fo), " + "the only clown to win a Nobel Prize in Literature." + "https://www.youtube.com/watch?v=TqKfwC70YZI"
+test_comment = "Test comment."
 
 # Create the Reddit instance
 reddit = praw.Reddit('bot1')
-
-# and login
-# with open("creds.txt", "r") as f:
-#     reddit.login(f[0], f[1])
-
-# if not os.path.isfile("comments_found.txt"):
-#     comments_found = []
 
 with open("comments_found.txt", "r") as f:
     comments_found = f.read()
@@ -37,7 +31,7 @@ with open("comments_found.txt", "r") as f:
 # let's pull comments from some subs!
 for sub in subs:
     subreddit = reddit.subreddit(sub)
-    for submission in subreddit.hot(limit=10):
+    for submission in subreddit.hot(limit=20):
         commentIterator = 0
         print("\n Scanning post in /r/" + subreddit.display_name + ": " + submission.title)
         submission.comments.replace_more(limit=4)
@@ -47,11 +41,16 @@ for sub in subs:
                 print("Comment " + str(commentIterator) + ": " + comment.body.lower())
                 thisComment = reddit.comment(comment.id)
                 if comment.id not in comments_found:
-                    # thisComment.reply(reply_text_2)
-                    # print("Test submission")
-                    print("Comment id:" + comment.id)
-                    comments_found.append(comment.id)
-                # thisComment.reply(body="It's still working!") // here's the reaction
+                    prompt = input("Clownish reply? \n ('y' to comment, 'n' to never comment, anything else to skip): ")
+                    if prompt == "y":
+                        thisComment.reply(reply_text)
+                        print("Comment left.")
+                        comments_found.append(comment.id)
+                    if prompt == "n":
+                        comments_found.append(comment.id)
+                        print("Comment ignored.")
+                    else:
+                        continue
             else:
                 pass
 
