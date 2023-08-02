bb0d5b8ac6e98c406b9ddb4a99136b9a7e7238ba
unknown <richard@richardhartnell.com>
2023-08-01 23:48:36 -0700

    File cleanup and renaming

--- clownbot.py
+++ clownbot.py
@@ -8,8 +8,8 @@ import os
 commentIterator = 0
 searchTerm = "clown"
 subs = ['news',
-        'politics',
-        'worldnews',
+        # 'politics',
+        # 'worldnews',
         'askreddit',
         'gaming',
         'videos',
@@ -34,7 +34,7 @@ subs = ['news',
 comments_found = []
  
 # the reply
-reply_text = "Hi! Circus performer here. This account is for roaming around Reddit trying to clear up three important things about this too-frequent comparison between clowns and stupid people:\n \n" \
+reply_text = "Hi! Circus performer here. Just dipping in to clear up this too-frequent comparison between clowns and stupid people:\n \n" \
              "1. Clowns are very diligent and work very hard at refining their art.\n \n"\
              "2. Clowns are generally very kind and well-intentioned people.\n \n" \
              "3. Clowns are only *pretending* they are completely stupid.\n \n" \
@@ -55,20 +55,21 @@ with open("authors_found.txt", "r") as g:
     authors_found = authors_found.split("\n")
     authors_found = list(filter(None, authors_found))
 
-
 # let's pull comments from some subs!
 while True:
     for sub in subs:
         subreddit = reddit.subreddit(sub)
         for submission in subreddit.hot(limit=20):
+            time.sleep(2)
             commentIterator = 0
             print("\n Scanning post in /r/" + subreddit.display_name + ": " + submission.title)
-            submission.comments.replace_more(limit=4)
+            submission.comments.replace_more(limit=3)
             for comment in submission.comments.list():
-                time.sleep(1)
+                time.sleep(2)
                 if commentIterator <= 12 and (str(searchTerm) in str(comment.body.lower())):
                     commentIterator += 1
                     thisComment = reddit.comment(comment.id)
+                    time.sleep(2)
                     if comment.id not in comments_found:
                         if str(comment.author) != 'clown_b0t':
                             print("Comment " + str(commentIterator) + ": " + comment.body.lower())
